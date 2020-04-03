b79d30ace28598730257f56b06406b786bae2ff8
Carl Lerche <me@carllerche.com>
2020-04-02 22:13:38 -0700

    Test key expiration

--- tests/server.rs
+++ tests/server.rs
@@ -4,6 +4,7 @@ use std::net::SocketAddr;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::task::JoinHandle;
+use tokio::time::{self, Duration};
 
 #[tokio::test]
 async fn basic_kv_usage() {
@@ -39,6 +40,50 @@ async fn basic_kv_usage() {
     stream.read_exact(&mut response).await.unwrap();
 
     assert_eq!(b"$5\r\nworld\r\n", &response);
+}
+
+#[tokio::test]
+async fn key_timeout() {
+    tokio::time::pause();
+
+    let (addr, _handle) = start_server().await;
+
+    // Establish a connection to the server
+    let mut stream = TcpStream::connect(addr).await.unwrap();
+
+    // Set a key
+    stream.write_all(b"*5\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld\r\n\
+                     +EX\r\n:1\r\n").await.unwrap();
+
+    let mut response = [0; 5];
+
+    // Read OK
+    stream.read_exact(&mut response).await.unwrap();
+
+    assert_eq!(b"+OK\r\n", &response);
+
+    // Get the key, data is present
+    stream.write_all(b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n").await.unwrap();
+
+    // Read "world" response
+    let mut response = [0; 11];
+
+    stream.read_exact(&mut response).await.unwrap();
+
+    assert_eq!(b"$5\r\nworld\r\n", &response);
+
+    // Wait for the key to expire
+    time::advance(Duration::from_secs(1)).await;
+
+    // Get a key, data is missing
+    stream.write_all(b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n").await.unwrap();
+
+    // Read nil response
+    let mut response = [0; 5];
+
+    stream.read_exact(&mut response).await.unwrap();
+
+    assert_eq!(b"$-1\r\n", &response);
 }
 
 async fn start_server() -> (SocketAddr, JoinHandle<mini_redis::Result<()>>) {
