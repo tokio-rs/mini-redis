c0408cbc014ace160fa601e46c36fdd77e6d3da8
Carl Lerche <me@carllerche.com>
2020-04-03 16:08:52 -0700

    Improve accept logic
    Handle accept errors using an exponential backoff strategy. Also uses a
    semaphore to limit the max number connections.
    
    Adds inline documentation and comments.

--- src/server.rs
+++ src/server.rs
@@ -1,34 +1,101 @@
+//! Minimal Redis server implementation
+//!
+//! Provides an async `run` function that listens for inbound connections,
+//! spawning a task per connection.
+
 use crate::{Command, Connection, Db, Shutdown};
 
 use std::future::Future;
-use tokio::net::TcpListener;
-use tokio::sync::broadcast;
+use std::sync::Arc;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{broadcast, Semaphore};
+use tokio::time::{self, Duration};
 use tracing::{debug, error, instrument, info};
 
+/// Server listener state. Created in the `run` call. It includes a `run` method
+/// which performs the TCP listening and initialization of per-connection state.
 #[derive(Debug)]
-struct Server {
-    /// Database state
+struct Listener {
+    /// Shared database handle.
+    ///
+    /// Contains the key / value store as well as the broadcast channels for
+    /// pub/sub.
+    ///
+    /// This is a wrapper around an `Arc`. This enables `db` to be cloned and
+    /// passed into the per connection state (`Handler`).
     db: Db,
 
-    /// TCP listener
+    /// TCP listener supplied by the `run` caller.
     listener: TcpListener,
 
-    /// Listen for shutdown
+    /// Limit the max number of connections.
+    ///
+    /// A `Semaphore` is used to limit the max number of connections. Before
+    /// attempting to accept a new connection, a permit is acquired from the
+    /// semaphore. If none are available, the listener waits for one.
+    ///
+    /// When handlers complete processing a connection, the permit is returned
+    /// to the semaphore.
+    limit_connections: Arc<Semaphore>,
+
+    /// Broadcasts a shutdown signal to all active connections.
+    ///
+    /// The initial `shutdown` trigger is provided by the `run` caller. The
+    /// server is responsible for gracefully shutting down active connections.
+    /// When a connection task is spawned, it is passed a broadcast receiver
+    /// handle. When a graceful shutdown is initiated, a `()` value is sent via
+    /// the broadcast::Sender. Each active connection receives it, reaches a
+    /// safe terminal state, and completes the task.
     notify_shutdown: broadcast::Sender<()>,
 }
 
-/// Handles a connections
+/// Per-connection handler. Reads requests from `connection` and applies the
+/// commands to `db`.
 #[derive(Debug)]
 struct Handler {
-    /// Database state
+    /// Shared database handle.
+    ///
+    /// When a command is received from `connection`, it is applied with `db`.
+    /// The implementation of the command is in the `cmd` module. Each command
+    /// will need to interact with `db` in order to complete the work.
     db: Db,
 
     /// The TCP connection decorated with the redis protocol encoder / decoder
+    /// implemented using a buffered `TcpStream`.
+    ///
+    /// When `Listener` receives an inbound connection, the `TcpStream` is
+    /// passed to `Connection::new`, which initializes the associated buffers.
+    /// `Connection` allows the handler to operate at the "frame" level and keep
+    /// the byte level protocol parsing details encapsulated in `Connection`.
     connection: Connection,
 
-    /// Listen for shutdown notifications
+    /// Max connection semaphore.
+    ///
+    /// When the handler is done processing, a permit is returned to this
+    /// semaphore. If the listener is waiting for connections to close, it will
+    /// be notified of the newly available permit and resume accepting
+    /// connections.
+    limit_connections: Arc<Semaphore>,
+
+    /// Listen for shutdown notifications.
+    ///
+    /// A wrapper around the `broadcast::Receiver` paired with the sender in
+    /// `Listener`. The connection handler processes requests from the
+    /// connection until the peer disconnects **or** a shutdown notification is
+    /// received from `shutdown`. In the latter case, any in-flight work being
+    /// processed for the peer is continued until it reaches a safe state, at
+    /// which point the connction is terminated.
     shutdown: Shutdown,
 }
+
+/// Maximum number of concurrent connections the redis server will accept.
+///
+/// When this limit is reached, the server will stop accepting connections until
+/// an active connection terminates.
+///
+/// A real application will want to make this value configurable, but for this
+/// example, it is hard coded.
+const MAX_CONNECTIONS: usize = 10_000;
 
 /// Run the mini-redis server.
 ///
@@ -44,9 +111,11 @@ pub async fn run(listener: TcpListener, shutdown: impl Future) -> crate::Result<
     // connections. When the provided `shutdown` future completes
     let (notify_shutdown, _) = broadcast::channel(1);
 
-    let mut server = Server {
+    // Initialize the listener state
+    let mut server = Listener {
         listener,
         db: Db::new(),
+        limit_connections: Arc::new(Semaphore::new(MAX_CONNECTIONS)),
         notify_shutdown,
     };
 
@@ -54,6 +123,21 @@ pub async fn run(listener: TcpListener, shutdown: impl Future) -> crate::Result<
     // server task runs until an error is encountered, so under normal
     // circumstances, this `select!` statement runs until the `shutdown` signal
     // is received.
+    //
+    // `select!` statements are written in the form of:
+    //
+    // ```
+    // <result of async op> = <async op> => <step to perform with result>
+    // ```
+    //
+    // All `<async op>` statements are executed concurrently. Once the **first**
+    // op completes, its associated `<step to perform with result>` is
+    // performed.
+    //
+    // The `select! macro is a foundational building block for writing
+    // asynchronous Rust. See the API docs for more details:
+    //
+    // https://docs.rs/tokio/*/tokio/macro.select.html
     tokio::select! {
         res = server.run() => {
             if let Err(err) = res {
@@ -69,20 +153,44 @@ pub async fn run(listener: TcpListener, shutdown: impl Future) -> crate::Result<
     Ok(())
 }
 
-impl Server {
+impl Listener {
     /// Run the server
     ///
     /// Listen for inbound connections. For each inbound connection, spawn a
     /// task to process that connection.
+    ///
+    /// # Errors
+    ///
+    /// Returns `Err` if accepting returns an error. This can happen for a
+    /// number reasons that resolve over time. For example, if the underlying
+    /// operating system has reached an internal limit for max number of
+    /// sockets, accept will fail.
+    ///
+    /// The process is not able to detect when a transient error resolves
+    /// itself. One strategy for handling this is to implement a back off
+    /// strategy, which is what we do here.
     async fn run(&mut self) -> crate::Result<()> {
         info!("accepting inbound connections");
 
         loop {
-            let (socket, _) = self.listener.accept().await?;
+            // Wait for a permit to become available
+            //
+            // `acquire` returns a permit that is bound via a lifetime to the
+            // semaphore. When the permit value is dropped, it is automatically
+            // returned to the sempahore. This is convenient in many cases.
+            // However, in this case, the permit must be returned in a different
+            // task than it is acquired in (the handler task). To do this, we
+            // "forget" the permit, which drops the permit value **without**
+            // incrementing the semaphore's permits. Then, in the handler task
+            // we manually add a new permit when processing completes.
+            self.limit_connections.acquire().await.forget();
+
+            let socket = self.accept().await?;
 
             let mut handler = Handler {
                 db: self.db.clone(),
                 connection: Connection::new(socket),
+                limit_connections: self.limit_connections.clone(),
                 shutdown: Shutdown::new(self.notify_shutdown.subscribe()),
             };
 
@@ -90,8 +198,31 @@ impl Server {
                 if let Err(err) = handler.run().await {
                     error!(cause = ?err, "connection error");
                 }
+
+                // Add a permit back to the semaphore.
+                handler.limit_connections.add_permits(1);
             });
         }
+    }
+
+    /// Accept an inbound connection.
+    ///
+    /// Errors are handled by backing off and retrying. Once the backoff exceeds
+    /// about 60 seconds, an error is returned.
+    async fn accept(&mut self) -> crate::Result<TcpStream> {
+        let mut last_err = None;
+
+        for i in 0..6 {
+            match self.listener.accept().await {
+                Ok((socket, _)) => return Ok(socket),
+                Err(err) => last_err = Some(err),
+            }
+
+            // Back off
+            time::delay_for(Duration::from_secs(1) * 2u32.pow(i)).await;
+        }
+
+        Err(last_err.unwrap().into())
     }
 }
 
