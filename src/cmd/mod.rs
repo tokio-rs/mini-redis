700e3ec8d9d19b554e7bec49d9655e75357dc19f
Guanqun Lu <guanqun.lu@gmail.com>
2020-05-18 23:38:58 +0800

    typo fix

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -46,7 +46,7 @@ impl Command {
         let mut parse = Parse::new(frame)?;
 
         // All redis commands begin with the command name as a string. The name
-        // is read and converted to lower casae in order to do case sensitive
+        // is read and converted to lower cases in order to do case sensitive
         // matching.
         let command_name = parse.next_string()?.to_lowercase();
 
