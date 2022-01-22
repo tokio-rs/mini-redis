8a8f7978b6559f69988ead77b13e61681c479e75
tison <wander4096@gmail.com>
2022-01-22 13:20:47 +0800

    support PING command for easy testing
    Signed-off-by: tison <wander4096@gmail.com>

---
+++ src/cmd/ping.rs
@@ -0,0 +1,70 @@
+use bytes::Bytes;
+use tracing::{debug, instrument};
+use crate::{Connection, Frame, Parse, ParseError};
+
+/// Returns PONG if no argument is provided, otherwise
+/// return a copy of the argument as a bulk.
+///
+/// This command is often used to test if a connection
+/// is still alive, or to measure latency.
+#[derive(Debug, Default)]
+pub struct Ping {
+    /// optional message to be returned
+    msg: Option<String>,
+}
+
+impl Ping {
+    /// Create a new `Ping` command with optional `msg`.
+    pub fn new(msg: Option<impl ToString>) -> Ping {
+        Ping {
+            msg: msg.map(|m| m.to_string()),
+        }
+    }
+
+    /// Parse a `Ping` instance from a received frame.
+    ///
+    /// The `Parse` argument provides a cursor-like API to read fields from the
+    /// `Frame`. At this point, the entire frame has already been received from
+    /// the socket.
+    ///
+    /// The `PING` string has already been consumed.
+    ///
+    /// # Returns
+    ///
+    /// Returns the `Ping` value on success. If the frame is malformed, `Err` is
+    /// returned.
+    ///
+    /// # Format
+    ///
+    /// Expects an array frame containing `PING` and an optional message.
+    ///
+    /// ```text
+    /// PING [message]
+    /// ```
+    pub(crate) fn parse_frames(parse: &mut Parse) -> crate::Result<Ping> {
+        match parse.next_string() {
+            Ok(msg) => Ok(Ping::new(Some(msg))),
+            Err(ParseError::EndOfStream) => Ok(Ping::default()),
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    /// Apply the `Ping` command and return the message.
+    ///
+    /// The response is written to `dst`. This is called by the server in order
+    /// to execute a received command.
+    #[instrument(skip(self, dst))]
+    pub(crate) async fn apply(self, dst: &mut Connection) -> crate::Result<()> {
+        let response = match self.msg {
+            None => Frame::Simple("PONG".to_string()),
+            Some(msg) => Frame::Bulk(Bytes::from(msg)),
+        };
+
+        debug!(?response);
+
+        // Write the response back to the client
+        dst.write_frame(&response).await?;
+
+        Ok(())
+    }
+}
