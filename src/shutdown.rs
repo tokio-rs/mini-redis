017a0df61feca8012251653dae10d1c3db89d151
Ting Sun <suntcrick@gmail.com>
2023-03-20 03:38:46 +0800

    too many 'shutdown' can be ambiguous

--- src/shutdown.rs
+++ src/shutdown.rs
@@ -12,7 +12,7 @@ use tokio::sync::broadcast;
 #[derive(Debug)]
 pub(crate) struct Shutdown {
     /// `true` if the shutdown signal has been received
-    shutdown: bool,
+    is_shutdown: bool,
 
     /// The receive half of the channel used to listen for shutdown.
     notify: broadcast::Receiver<()>,
@@ -22,21 +22,21 @@ impl Shutdown {
     /// Create a new `Shutdown` backed by the given `broadcast::Receiver`.
     pub(crate) fn new(notify: broadcast::Receiver<()>) -> Shutdown {
         Shutdown {
-            shutdown: false,
+            is_shutdown: false,
             notify,
         }
     }
 
     /// Returns `true` if the shutdown signal has been received.
     pub(crate) fn is_shutdown(&self) -> bool {
-        self.shutdown
+        self.is_shutdown
     }
 
     /// Receive the shutdown notice, waiting if necessary.
     pub(crate) async fn recv(&mut self) {
         // If the shutdown signal has already been received, then return
         // immediately.
-        if self.shutdown {
+        if self.is_shutdown {
             return;
         }
 
@@ -44,6 +44,6 @@ impl Shutdown {
         let _ = self.notify.recv().await;
 
         // Remember that the signal has been received.
-        self.shutdown = true;
+        self.is_shutdown = true;
     }
 }
