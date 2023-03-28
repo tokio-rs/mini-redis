74bc0261cb750e45ea8b849e765de616eb156a2b
Ting Sun <suntcrick@gmail.com>
2023-03-29 03:27:56 +0800

    collect all clients to one single folder

--- src/clients/blocking_client.rs
+++ src/clients/blocking_client.rs
@@ -89,7 +89,7 @@ impl BlockingClient {
     /// Demonstrates basic usage.
     ///
     /// ```no_run
-    /// use mini_redis::blocking_client;
+    /// use mini_redis::clients::blocking_client;
     ///
     /// fn main() {
     ///     let mut client = blocking_client::connect("localhost:6379").unwrap();
@@ -115,7 +115,7 @@ impl BlockingClient {
     /// Demonstrates basic usage.
     ///
     /// ```no_run
-    /// use mini_redis::blocking_client;
+    /// use mini_redis::clients::blocking_client;
     ///
     /// fn main() {
     ///     let mut client = blocking_client::connect("localhost:6379").unwrap();
@@ -149,7 +149,7 @@ impl BlockingClient {
     /// favorable.
     ///
     /// ```no_run
-    /// use mini_redis::blocking_client;
+    /// use mini_redis::clients::blocking_client;
     /// use std::thread;
     /// use std::time::Duration;
     ///
@@ -191,7 +191,7 @@ impl BlockingClient {
     /// Demonstrates basic usage.
     ///
     /// ```no_run
-    /// use mini_redis::blocking_client;
+    /// use mini_redis::clients::blocking_client;
     ///
     /// fn main() {
     ///     let mut client = blocking_client::connect("localhost:6379").unwrap();
